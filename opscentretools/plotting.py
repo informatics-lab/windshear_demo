41946839d4d8dd3452fd2d3d55c36c960fefdf3d
Kevin Donkers <kevin.donkers@informaticslab.co.uk>
2019-06-28 16:43:39 +0000

    plotting library added to tools

---
+++ opscentretools/plotting.py
@@ -0,0 +1,34 @@
+import holoviews as hv
+import geoviews as gv
+import cartopy.crs as ccrs
+import cartopy.feature as cf
+from holoviews.operation.datashader import regrid
+import panel as pn
+pn.extension()
+hv.extension('bokeh')
+
+def interactive_plot(cube, cmap='viridis', coastlines=True, coastline_color='grey', projection=ccrs.PlateCarree, tools=['hover'], min_height=600, **opts):
+    # Generate an interactive Bokeh image of a cube with various plotting options
+    
+    # Convert cube to GeoViews dataset
+    dataset = gv.Dataset(cube, [coord.name() for coord in cube.dim_coords])
+    
+    # Generate an image object which will dynamically render as the interactive view changes
+    image = regrid(dataset.to(gv.Image, ['longitude', 'latitude'], dynamic=True))
+    
+    # Options for plotting
+    options = {
+        'cmap': cmap,        
+        'responsive': True,
+        'projection': projection(),
+        'colorbar': True,
+        'min_height': min_height,
+        'aspect': 2,
+        'tools': tools
+    }
+    
+    # Include coastlines if needed
+    if coastlines:
+        return image.opts(**options, **opts) * gv.feature.coastline.opts(line_color=coastline_color)
+    else:
+        return image.opts(**options, **opts)
