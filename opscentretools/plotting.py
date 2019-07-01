7913f937c1afffc24c8765ac04cfe07b20de7ada
Kevin Donkers <kevin.donkers@informaticslab.co.uk>
2019-07-01 13:39:21 +0000

    add linked slider panel dashboard

--- opscentretools/plotting.py
+++ opscentretools/plotting.py
@@ -7,7 +7,7 @@ import panel as pn
 pn.extension()
 hv.extension('bokeh')
 
-def interactive_plot(cube, cmap='viridis', coastlines=True, coastline_color='grey', projection=ccrs.PlateCarree, tools=['hover'], min_height=600, **opts):
+def interactive_plot(cube, cmap='viridis', coastlines=True, coastline_color='white', projection=ccrs.PlateCarree, tools=['hover'], min_height=600, **opts):
     # Generate an interactive Bokeh image of a cube with various plotting options
     
     # Convert cube to GeoViews dataset
@@ -32,3 +32,25 @@ def interactive_plot(cube, cmap='viridis', coastlines=True, coastline_color='gre
         return gv.feature.ocean * gv.feature.land * image.opts(**options, **opts) * gv.feature.coastline.opts(line_color=coastline_color)
     else:
         return image.opts(**options, **opts)
+    
+def two_plot_linked_slider(plot1, plot2):
+    # Generate a Panel dashboard of two plots with a shared slider
+    
+    # Create a Panel object to host our plots
+    app = pn.GridSpec(sizing_mode='stretch_both')
+    
+    # Arrange plots in a column
+    plots = pn.Column(plot1, plot2)
+    
+    # Pull out the sliders from each plot and link them
+    slider1 = plots[0][1][0]
+    slider2 = plots[1][1][0]
+    slider1.link(slider2, value='value')
+    slider1.name = slider1.name.capitalize()
+    
+    # Arrange the plots vertically with a shared slider in the right hand quarter
+    app[0, 0:4]=plots[0][0]
+    app[1, 0:4]=plots[1][0]
+    app[0, 4]=slider1
+    
+    return app
