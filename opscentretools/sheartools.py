4a3364c6b6bb6411dec5d5bfe55dc3862903f938
kaedonkers <kaedonkers@local>
2019-07-02 15:56:36 +0000

    shear calculations now work for height and pressure

--- opscentretools/sheartools.py
+++ opscentretools/sheartools.py
@@ -9,13 +9,24 @@ import cartopy.crs as ccrs
 import cartopy.feature as cf
 import numpy as np
 
-def boundry_presure_levels(cube):
+
+def vertical_boundary_levels(cube, vertical='pressure'):
+    # Calculate the level values between each pair of levels
+    if vertical=='pressure':
+        return boundary_pressure_levels(cube)
+    elif vertical=='height':
+        return boundary_height_levels(cube)
+    else:
+        raise Exception('Vertical boundary layers cannot be calculated for "{}"'.format(vertical))
+
+
+def boundary_pressure_levels(cube):
     upper = cube.extract(
                 iris.Constraint(
                     pressure=lambda p:p < cube.coord('pressure').points[0]))
     lower = cube.extract(
-                    iris.Constraint(
-                        pressure=lambda p:p > cube.coord('pressure').points[-1]))
+                iris.Constraint(
+                    pressure=lambda p:p > cube.coord('pressure').points[-1]))
     
     new_levels = (upper.coord('pressure').points +  lower.coord('pressure').points )/2
     for cube in [upper, lower]:
@@ -24,19 +35,49 @@ def boundry_presure_levels(cube):
     return upper, lower
 
 
-def calculate_shear(wind_speed, wind_dir):
-    # Calculate the cross-product of wind vectors between consecutive pressure levels
+def boundary_height_levels(cube):
+    upper = cube.extract(
+                iris.Constraint(
+                    height=lambda h:h > cube.coord('height').points[0]))
+    lower = cube.extract(
+                iris.Constraint(
+                    height=lambda h:h < cube.coord('height').points[-1]))
     
+    new_levels = (upper.coord('height').points +  lower.coord('height').points )/2
+    for cube in [upper, lower]:
+        cube.coord('height').points = new_levels
+        cube.coord('height').bounds = None
+    return upper, lower
+
+
+def calculate_shear(wind_speed, wind_dir):
+    # Calculate shear of cubes after asserting whether they have vertical 
+    
+    # determine whether vertical dims are pressure or height
+    dir_dims = [dim.var_name for dim in wind_dir.dim_coords]
+    speed_dims = [dim.var_name for dim in wind_speed.dim_coords]
+    
+    # calculate shear if either pressure or height are present in the supplied cubes
+    if 'pressure' in (dir_dims and speed_dims):
+        return vertical_shear_calculation(wind_speed, wind_dir, vertical='pressure')
+    elif 'height' in (dir_dims and speed_dims):
+        return vertical_shear_calculation(wind_speed, wind_dir, vertical='height')
+    else:
+        raise Exception('Cubes do not contain either "pressure" or "height" dim_coords')
+    
+    
+def vertical_shear_calculation(wind_speed, wind_dir, vertical='pressure'):
+    # Calculate the cross-product of wind vectors between consecutive pressure or height levels
     # lets ensure we are working on the same coords on both cubes
-    assert wind_speed.coord_dims('pressure') == wind_dir.coord_dims('pressure')
+    assert wind_speed.coord_dims(vertical) == wind_dir.coord_dims(vertical)
     
     # make copies of cubes
     speed = wind_speed.copy()
     direction = wind_dir.copy()
     
     # work out the parameters for the 
-    speed_lower, speed_upper = boundry_presure_levels(speed)
-    dir_lower, dir_upper = boundry_presure_levels(direction)
+    speed_lower, speed_upper = vertical_boundary_levels(speed, vertical)
+    dir_lower, dir_upper = vertical_boundary_levels(direction, vertical)
     
     x_lower = speed_lower * iris.analysis.maths.apply_ufunc(np.cos, dir_lower)
     y_lower = speed_lower * iris.analysis.maths.apply_ufunc(np.sin, dir_lower)
@@ -52,6 +93,7 @@ def calculate_shear(wind_speed, wind_dir):
     shear.units = "m s-1"
     
     return shear
+
 
 def calculate_ensemble_exceedence(wind_shear, threshold=15):
     # Calculate the proportion of ensemble members which have shear values exceeding the threshold
